bd37afced0cc496bf52a1c300f6d1a6cdbeafb47
yu-ec28100h <ec28100h@gmail.com>
2021-07-12 12:49:04 +0900

    ゲームシーン、クリアシーンの実装

---
+++ Assets/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+
+    void Start()
+    {
+        this.player = GameObject.Find("cat");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(transform.position.x,
+                                         playerPos.y,
+                                         transform.position.z);
+    }
+}
